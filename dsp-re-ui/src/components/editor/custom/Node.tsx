dcc6d850a62656956b4bd13b45a5d7e709fc39f9
Sholto Armstrong <sjnarmstrong@gmail.com>
2024-12-12 14:00:41 +0200

    Remove buttons off nodes

--- dsp-re-ui/src/components/editor/custom/Node.tsx
+++ dsp-re-ui/src/components/editor/custom/Node.tsx
@@ -2,8 +2,6 @@ import * as React from 'react'
 import { ClassicScheme, RenderEmit, Presets } from "rete-react-plugin";
 import styled, { css } from "styled-components";
 import { $nodewidth, $socketmargin, $socketsize } from "./vars";
-import { Trash2, PlusCircle } from 'lucide-react';
-import { Button } from '@/components/ui/button';
 
 import {
   Card,
@@ -40,13 +38,7 @@ type Props<S extends ClassicScheme> = {
 }
 export type NodeComponent<Scheme extends ClassicScheme> = (props: Props<Scheme>) => JSX.Element
 
-const onDelete = (id: string) => {
-  console.log(id)
-}
-const onAddChild = (id: string) => {
-  console.log(id)
 
-}
 export function CustomNode<Scheme extends ClassicScheme>(props: Props<Scheme>) {
   const inputs = Object.entries(props.data.inputs)
   const outputs = Object.entries(props.data.outputs)
@@ -69,42 +61,84 @@ export function CustomNode<Scheme extends ClassicScheme>(props: Props<Scheme>) {
         flex-col
         items-center
         justify-center
+        w-[${width}px]
+        h-[${height}px]
         overflow-hidden
         ${selected ? 'ring-2 ring-primary/70' : ''}
       `}
-      style={{
-        width: `${width}px`,
-        height: `${height}px`
-      }}
+      data-testid="node"
+      key={id}
+  >
+      <div className="absolute top-0 left-0 right-0 flex items-center justify-center px-2 -translate-y-1/2">
+      {inputs.map(([key, input]) => input && (
+          <RefSocket
+            name="input-socket"
+            side="input"
+            socketKey={key}
+            nodeId={id}
+            emit={props.emit}
+            payload={input.socket}
+            data-testid="input-socket"
+            key={key}
+          />
+        ))}
+      </div>
+      <div 
+        className="
+          text-center 
+          py-2 
+          px-4 
+          font-semibold 
+        " 
+        data-testid="title"
+      >
+        {label}
+      </div>
+      <div className="absolute bottom-0 left-0 right-0 flex items-center justify-center px-2 translate-y-1/2">
+      {outputs.map(([key, output]) => output && (
+          <RefSocket
+            name="output-socket"
+            side="output"
+            socketKey={key}
+            nodeId={id}
+            emit={props.emit}
+            payload={output.socket}
+            data-testid="output-socket"
+            key={key}
+          />
+        ))}
+      </div>
+
+  </div>
+  )
+}
+
+export function CustomNode4<Scheme extends ClassicScheme>(props: Props<Scheme>) {
+  const inputs = Object.entries(props.data.inputs)
+  const outputs = Object.entries(props.data.outputs)
+  const selected = props.data.selected || false
+  const { id, label } = props.data
+
+  sortByIndex(inputs)
+  sortByIndex(outputs)
+
+  return (
+    <div 
+      className={`
+        relative 
+        bg-accent-foreground 
+        text-accent
+        rounded-lg 
+        border 
+        border-primary/50 
+        shadow-sm
+        ${selected ? 'ring-2 ring-primary/70' : ''}
+      `}
       data-testid="node"
       key={id}
     >
-      {/* Delete Button */}
-      {selected && onDelete && (
-        <Button 
-          variant="destructive" 
-          size="icon" 
-          className="absolute top-1 left-1 z-10 w-6 h-6"
-          onClick={() => onDelete(id)}
-        >
-          <Trash2 className="w-4 h-4" />
-        </Button>
-      )}
-
-      {/* Add Child Button */}
-      {selected && onAddChild && (
-        <Button 
-          variant="outline" 
-          size="icon" 
-          className="absolute top-1 right-1 z-10 w-6 h-6"
-          onClick={() => onAddChild(id)}
-        >
-          <PlusCircle className="w-4 h-4" />
-        </Button>
-      )}
-
-      {/* Input Sockets */}
-      <div className="absolute top-0 left-0 right-0 flex items-center justify-center px-2 -translate-y-1/2">
+      {/* Input Sockets - Positioned evenly across top border */}
+      <div className="absolute top-0 left-0 right-0 flex justify-between px-2 -translate-y-1/2">
         {inputs.map(([key, input]) => input && (
           <RefSocket
             name="input-socket"
@@ -119,21 +153,23 @@ export function CustomNode<Scheme extends ClassicScheme>(props: Props<Scheme>) {
         ))}
       </div>
 
-      {/* Node Label */}
+      {/* Node Title */}
       <div 
         className="
           text-center 
           py-2 
           px-4 
           font-semibold 
+          border-b 
+          border-primary/20
         " 
         data-testid="title"
       >
         {label}
       </div>
 
-      {/* Output Sockets */}
-      <div className="absolute bottom-0 left-0 right-0 flex items-center justify-center px-2 translate-y-1/2">
+      {/* Output Sockets - Positioned evenly across bottom border */}
+      <div className="absolute bottom-0 left-0 right-0 flex justify-between px-2 translate-y-1/2">
         {outputs.map(([key, output]) => output && (
           <RefSocket
             name="output-socket"
